1f3d36aa8796ca7b27e97add58ba505a1d703f19
PavelGirkalo <pashe4ka_86@mail.ru>
2017-03-11 16:38:43 +0400

    Create and use function distance

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -11,11 +11,17 @@ public class MyFirstProgram {
     Rectangle r = new Rectangle(5, 10);
     System.out.println("Square of a rectangle with sides " + r.a + " and " + r.b + " = " + r.area());
 
+    Point p1 = new Point(2.5, 3.5);
+    Point p2 = new Point(5.5, -0.5);
+    System.out.println("Distance between p1(" + p1.a + ";" + p1.b + ") and p2(" + p2.a + ";" + p2.b + ") = " + distance(p1, p2));
   }
 
   public static void hello(String string) {
-
     System.out.println("Hello, " + string + "!");
+  }
+
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt((p1.a - p2.a)*(p1.a - p2.a)+(p1.b - p2.b)*(p1.b - p2.b));
   }
 
 }
