f0212186ec9d3880906c2dca7510b89743c6f5af
PavelGirkalo <pashe4ka_86@mail.ru>
2017-03-11 16:02:39 +0400

    Создание новых классов

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,26 +4,18 @@ public class MyFirstProgram {
   public static void main(String[] args) {
     hello("world");
     hello("Pavel");
-    double l = 16;
-    double a = 10;
-    double b = 6;
 
-    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+    Rectangle r = new Rectangle(5, 10);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
   }
 
   public static void hello(String string) {
 
     System.out.println("Hello, " + string + "!");
-  }
-
-  public static double area(double len){
-    return len*len;
-  }
-
-  public static double area(double a, double b){
-    return a*b;
   }
 
 }
