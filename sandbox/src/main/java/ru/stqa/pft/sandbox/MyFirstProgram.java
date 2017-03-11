95a459c412940be903f42198c5b01a3e45093389
PavelGirkalo <pashe4ka_86@mail.ru>
2017-03-11 17:03:32 +0400

    Refactoring

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,7 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
+    /*
     hello("world");
     hello("Pavel");
 
@@ -10,10 +11,14 @@ public class MyFirstProgram {
 
     Rectangle r = new Rectangle(5, 10);
     System.out.println("Square of a rectangle with sides " + r.a + " and " + r.b + " = " + r.area());
+    */
 
+    //Вычисление расстояния между точками с помощью функции distance()
     Point p1 = new Point(2.5, 3.5);
     Point p2 = new Point(5.5, -0.5);
+    System.out.println("Calculate distance using function");
     System.out.println("Distance between p1(" + p1.a + ";" + p1.b + ") and p2(" + p2.a + ";" + p2.b + ") = " + distance(p1, p2));
+
   }
 
   public static void hello(String string) {
