a8006de5a7a12b836dda54f083f86c102da01097
PavelGirkalo <pashe4ka_86@mail.ru>
2017-03-18 01:35:01 +0400

    Создание класса ApplicationManager и его помощников

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -0,0 +1,55 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+
+  FirefoxDriver wd;
+
+  private SessionHelper sessionHelper;
+  private NavigationHelper navigationHelper;
+  private GroupHelper groupHelper;
+  private ContactHelper contactHelper;
+
+
+  public static boolean isAlertPresent(FirefoxDriver wd) {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  public void init() {
+    wd = new FirefoxDriver();
+    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/");
+    sessionHelper = new SessionHelper(wd);
+    navigationHelper = new NavigationHelper(wd);
+    groupHelper = new GroupHelper(wd);
+    contactHelper = new ContactHelper(wd);
+    sessionHelper.login("admin", "secret");
+  }
+
+
+  public void stop() {
+    wd.quit();
+  }
+
+  public GroupHelper getGroupHelper() {
+    return groupHelper;
+  }
+
+  public ContactHelper getContactHelper() {
+    return contactHelper;
+  }
+
+  public NavigationHelper getNavigationHelper() {
+    return navigationHelper;
+  }
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactCreationTests extends TestBase {
+
+
+  @Test
+  public void testContactCreation() {
+    app.getContactHelper().initContactCreation();
+    app.getContactHelper().fillContactForm(new ContactData("First", "Middle", "Last", "Nickname", "Title", "Company", "Address", "+79010000001", "test@gmail.com", "homepage.com", "3", "2", "1950"));
+    app.getContactHelper().submitContactCreation();
+    app.getNavigationHelper().returnHomepage();
+  }
+
+}
