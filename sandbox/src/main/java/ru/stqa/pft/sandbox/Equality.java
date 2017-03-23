adab50051c01e8b76dab5a06fb97a06dfe943ccb
PavelGirkalo <pashe4ka_86@mail.ru>
2017-03-23 21:21:33 +0400

    Добавление класса и тестов для вычисления корней
    Добавление класса и тестов для вычисления числа корней уравнения

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equality.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.sandbox;
+
+import java.util.Objects;
+
+public class Equality {
+
+  public static void main(String[] args) {
+    String s1 = "firefox";
+    String s2 = "fire" + "fox";
+
+    System.out.println(s1 == s2);
+    System.out.println(s1.equals(s2));
+    //System.out.println(Objects.equals(s1,s2));
+
+  }
+}
