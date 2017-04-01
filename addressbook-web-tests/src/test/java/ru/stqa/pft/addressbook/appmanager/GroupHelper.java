96301ab0855508588efcf9e7c5b545f29f878e1a
PavelGirkalo <pashe4ka_86@mail.ru>
2017-04-01 11:42:59 +0400

    Рефакторинг кода тестов групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.List;
 
