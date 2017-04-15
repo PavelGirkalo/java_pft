a7757b9c3e2e84fe68a6a51f35d24171b2feb7d1
PavelGirkalo <pashe4ka_86@mail.ru>
2017-04-15 11:28:22 +0400

    Параметризация тестов создания групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,27 +1,42 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
+  @DataProvider
+  public Iterator<Object[]> validContacts() throws IOException {
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader (new FileReader(new File("src/test/resources/contacts.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[] {new ContactData().withFirstname(split[0]).withLastname(split[1]).withAddress(split[2]).withEmail(split[3]).withMobile(split[4])});
+      line = reader.readLine();
+    }
 
-  @Test(enabled = true)
-  public void testContactCreation() {
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) {
     app.goTo().homepage();
     Contacts before = app.contact().all();
     File photo = new File("src/test/resources/st.jpg");
-    ContactData contact = new ContactData()
-            .withFirstname("First").withLastname("Last").withPhoto(photo)
-            .withHome("111-111").withMobile("+79010000001").withWork("666 666")
-            .withAddress("Address")
-            .withEmail("email@gmail.com").withEmail2("email2@gmail.com").withEmail3("email3@gmail.com");
     app.contact().create(contact);
     app.goTo().homepage();
     Contacts after = app.contact().all();
