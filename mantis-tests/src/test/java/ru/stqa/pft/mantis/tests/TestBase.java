e75aa09b83de1f0aaea9dc953845c06838d051c2
PavelGirkalo <pashe4ka_86@mail.ru>
2017-05-05 23:54:13 +0400

    Добавление инеграции тестов и Mantis-трекера

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -1,12 +1,17 @@
 package ru.stqa.pft.mantis.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 
+import javax.xml.rpc.ServiceException;
 import java.io.File;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
 
 public class TestBase {
 
@@ -23,6 +28,21 @@ public class TestBase {
   public void tearDown() throws IOException {
     app.ftp().restore("config_inc.php.bak", "config_inc.php");
     app.stop();
+  }
+
+  public void skipIfNotFixed(int issueId) throws RemoteException, ServiceException, MalformedURLException {
+    if (isIssueOpen(issueId)) {
+      throw new SkipException("Ignored because of issue " + issueId);
+    }
+  }
+
+  private boolean isIssueOpen(int issueId) throws RemoteException, ServiceException, MalformedURLException {
+    String status = app.soap().getIssueWithId(BigInteger.valueOf(issueId)).getStatus();
+    if (status.equals("resolved")) {
+      return false;
+    } else {
+      return true;
+    }
   }
 
 }
